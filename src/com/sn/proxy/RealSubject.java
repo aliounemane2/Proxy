c1505c648601206f4d96e857d35cb5054bcf1135
Alioune MANE <aliounemane@finetechgroupe.com>
2022-01-16 15:52:33 +0000

    Design Pattern Proxy

---
+++ src/com/sn/proxy/RealSubject.java
@@ -0,0 +1,14 @@
+package com.sn.proxy;
+
+/**
+ * RealSubject class is a real object which is represented by Proxy
+ *
+ */
+public class RealSubject implements Subject {
+
+
+    @Override
+    public void doOperation() {
+
+    }
+}
---
+++ src/com/sn/proxy/Subject.java
@@ -0,0 +1,11 @@
+package com.sn.proxy;
+
+/**
+ * Subject interface defines common interface for RealSubject and Proxy
+ *
+ */
+public interface Subject {
+
+    void doOperation();
+
+}
